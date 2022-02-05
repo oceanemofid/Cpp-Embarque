a988bc157a6bc24f7efb644212a817b967f4b60f
oceanemofid <oceane.mofid@gmail.com>
2022-02-05 01:03:44 +0100

    continuation

--- TP1/histogram.cpp
+++ TP1/histogram.cpp
@@ -45,29 +45,65 @@ using std::vector;
 // This program has several problems, can you spot them
 
 int main(int argc, char *argv[]) {
+
   string file_name{argv[1]};
-  vector<double> buf;
-  vector<double> val;
   std::ifstream fin(file_name, std::ios::in);
 
+  vector<double> buf;
+  vector<int> all_sizes; //vecteur contenant les size des subvecteurs
+  int maxi = 0;
   string line;
   auto mean = 0.0;
 
   while (std::getline(fin, line)) {
     auto d = std::stod(line);
-    buf.push_back(d);
-    mean = (buf.size() == 1) ? d : mean + (d - mean) / buf.size();
+    //si dans l'intervalle [0;7999.9] on ajoute au buffer et on calcule la moyenne 
+    if(d < 7999.99){
+      buf.push_back(d);
+      //moyenne
+      mean = (buf.size() == 1) ? d : mean + (d - mean) / buf.size();
+    }
+    
   }
 
-  std::sort(buf.begin(), buf.end());
-  //val.assign(buf.begin(), buf.begin() +
-  //erase  
-
+  //médiane
+  //on trie le vector par ordre croissant 
+  std::sort(buf.begin(), buf.end()); 
   auto mid = buf.size() / 2;
+  int t = buf.size();
   double median = (buf.size() % 2) ? buf[mid] :
                                      (buf[mid - 1] + buf[mid]) / 2;
 
+  //construction des brackets
+  vector<double> subvector;
+  for(int i = 0; i<80; ++i){
+    for(int m = 0; m<t; ++m){
+    //on met dans un sous vector si la valeur est dans l'intervalle
+      if(buf[i]<100*i+100 && buf[i]>100*i){
+        subvector.push_back(buf[i]);
+      }
+    }
+    //on récupère la taille du subvecteur
+    int taille = subvector.size();
+    //on compare la size de la bracket actuelle au max précédent
+    maxi = std::max(maxi,taille);
+    all_sizes.push_back(taille); //on stocke la size du subvecteur
+  }
+  //infos 
   std::cout << "number of elements = " << buf.size()
             << ", median = " << median
             << ", mean = " << mean << std::endl;
+
+  //affichage
+  //affiche le bracket inf puis le nbr d'éléments de la bracket puis les étoiles
+  for(int i = 0; i<80 ; ++i){
+    std::cout << i << " " << all_sizes[i] << " ";
+    int nbr = int((all_sizes[i]*60)/maxi);
+    //print le bon nbr d'étoiles
+    for(int j = 0; j < nbr; ++j){
+      std::cout << "*";
+    }
+    std::cout << std::endl;
+  }
+  
 }
