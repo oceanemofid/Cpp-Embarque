b52872c9cf359d649aeeeb09071be425a612d055
oceanemofid <oceane.mofid@gmail.com>
2022-02-07 14:11:38 +0100

    création prgm 2

---
+++ TP1/map1.cpp
@@ -0,0 +1,34 @@
+//
+// header-start
+//////////////////////////////////////////////////////////////////////////////////
+//
+// \file      histogram
+//
+// \brief     This file belongs to the C++ tutorial project
+//
+// \author    Jonas Audrey, Ghobrial Sara, Mofid Océane
+//
+// \copyright Copyright ng2goodies 2015
+//            Distributed under the MIT License
+//            See http://opensource.org/licenses/MIT
+//
+//////////////////////////////////////////////////////////////////////////////////
+// header-end
+//
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+
+using std::string;
+using std::vector;
+
+// This simple program reads from a file a set of numbers (double format)
+// computes a running mean value, computes the median after sort
+// and prints a histogram af all values between 0 and 7999.99 placed into brackets
+
+int main(int argc, char *argv[]) {
+}
