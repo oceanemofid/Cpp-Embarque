6b05142df78cb6746dda23be2fdc2fa34d7514fb
oceanemofid <oceane.mofid@gmail.com>
2022-02-07 15:28:10 +0100

    update

--- TP1/map1.cpp
+++ TP1/map1.cpp
@@ -2,11 +2,11 @@
 // header-start
 //////////////////////////////////////////////////////////////////////////////////
 //
-// \file      histogram
+// \file      map1
 //
 // \brief     This file belongs to the C++ tutorial project
 //
-// \author    Jonas Audrey, Ghobrial Sara, Mofid Océane
+// \author    Ghobrial Sara, Jonas Audrey, Mofid Océane
 //
 // \copyright Copyright ng2goodies 2015
 //            Distributed under the MIT License
@@ -17,14 +17,13 @@
 //
 
 #include <string>
-#include <vector>
 #include <fstream>
 #include <iostream>
-#include <algorithm>
 #include <iomanip>
+#include <sstream>
+#include <map>
 
 using std::string;
-using std::vector;
 
 // This simple program reads from a file a set of numbers (double format)
 // computes a running mean value, computes the median after sort
@@ -35,16 +34,36 @@ int main(int argc, char *argv[]) {
     string file_name{argv[1]};
     std::ifstream fin(file_name, std::ios::in);
     string line;
+    string qin;
+    std::map<std::string, double> identifierKey;
 
-    std::cout << "query > ";
-    std::string query;
-    std::cin >> query;
+    while(qin.compare("END")){
+        std::cout << "query > ";
+        std::cin >> qin;
     
-    while (std::getline(fin, line)) {
-        std::cout << line << std::endl;
+        while (std::getline(fin, line)) {
+
+            std::istringstream stream(line);
+            string s;
+            double f;
+            stream >> s >> f ;
+            identifierKey.insert({s, f});
+            // extract the rest using the streambuf overload
+            stream >> std::cout.rdbuf();
+
+        }
+            if(identifierKey.find(qin) != identifierKey.end()){
+                std::cout << "value[" << identifierKey.find(qin)->first << "] = "
+                << identifierKey.find(qin)->second << std::endl;
+            }
+
+            else {
+                if(qin == "END"){
+                    break;
+                }
+                std::cout << "This ID does not exist" << std::endl;
+            }
+        
     }
-    
-
-
-
+    std::cout << "Bye..." << std::endl;
 }
